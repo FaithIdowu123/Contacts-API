5c4d325d0e6de756a3608fd8a622f51cf0d202d7
ofaith@byupathway.edu <ofaith@byupathway.edu>
2026-01-10 19:56:43 +0100

    fix

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Contacts API',
     description: 'A simple Contacts API',
   },
-  host: 'https://contacts-api-v6de.onrender.com/api',
+  host: '//contacts-api-v6de.onrender.com/api',
   schemes: ['https'],
 };  
 const outputFile = './swagger.json';
